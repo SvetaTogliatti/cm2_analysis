3d98ab7cb3ba470d85684462e83d1737f3852c2b
SvetaTogliatti <sveta.togliatti@gmail.com>
2024-09-12 00:18:34 +0300

    new app

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ import matplotlib.pyplot as plt
 
 # Исходные данные
 data = [
-    ['Skyeng English Old',122613,0.1577,0.6667,0.5,0.937,51124.22,23161.03,0.0,
-     0.008,62429.84,0.15,9,0.2903,0.3676,0.0853,0,0,0.0181,285,3579],
-    ['Skyeng English Courses & Programmes',216119,0.0427,0.7231,0.9344,0.937,116033.72,35039.72,0.0,
-     0.008,130281.04,0.15,9,0.2903,0.3676,0.0853,0.00116,0.17,0.0404,5234,2937],
-    ['Skysmart School',247997,0.0926,0.7593,0.9359,0.937,82208.96,15174.63,0.0,
-     0.008,110584.93,0.15,9,0.2903,0.3676,0.0853,0.002432,0.13,0.0191,9143,5178],
-    ['Skysmart Homeschooling',21994,0.0507,0.7415,0.9311,0.937,99869.91,14751,0.0,
-     0.008,66940.63,0.15,9,0.2903,0.3676,0.0853,0.001712,0.25,0.0544,876,79],
-    ['Skysmart Preschool',25746,0.0932,0.7631,0.9535,0.937,70825.75,20140.36,0.0,
-     0.008,84901.29,0.15,9,0.2903,0.3676,0.0853,0.00188,0.2,0.0368,1306,357],
-    ['Skysmart Coding',62258,0.0930,0.7815,0.9516,0.937,101590.19,24171.89,0.0,
-     0.008,128775.13,0.15,9,0.2903,0.3676,0.0853,0.001176,0.17,0.0326,3821,862],
-    ['Skysmart Exams',41348,0.1210,0.7628,0.9463,0.937,91172.79,24851.56,0.0,
-     0.008,98266.09,0.15,9,0.2903,0.3676,0.0853,0.002848,0.11,0.0197,2250,603],
-    ['Skysmart Exams Large Classes',19021,0.0879,0.8361,0.9391,0.937,50645.17,6888.04,0.0,
-     0.008,31109.05,0.15,9,0.2903,0.3676,0.0853,0.001824,0.14,0.0356,896,179]
+    ['Skyeng English Old',122613,0.1577,0.6667,0.5,0.937,51124.22,23161.03,0.06,
+     0.008,62429.84,0.15,9,0.2903,0.3676,0.0853,0,0,0.0181,285,3579,0.89],
+    ['Skyeng English Courses & Programmes',216119,0.0427,0.7231,0.9344,0.937,116033.72,35039.72,0.10,
+     0.008,130281.04,0.15,9,0.2903,0.3676,0.0853,0.00116,0.17,0.0404,5234,2937,0.6],
+    ['Skysmart School',247997,0.0926,0.7593,0.9359,0.937,82208.96,15174.63,0.09,
+     0.008,110584.93,0.15,9,0.2903,0.3676,0.0853,0.002432,0.13,0.0191,9143,5178,0.55],
+    ['Skysmart Homeschooling',21994,0.0507,0.7415,0.9311,0.937,99869.91,14751,0.10,
+     0.008,66940.63,0.15,9,0.2903,0.3676,0.0853,0.001712,0.25,0.0544,876,79,0.59],
+    ['Skysmart Preschool',25746,0.0932,0.7631,0.9535,0.937,70825.75,20140.36,0.10,
+     0.008,84901.29,0.15,9,0.2903,0.3676,0.0853,0.00188,0.2,0.0368,1306,357,0.59],
+    ['Skysmart Coding',62258,0.0930,0.7815,0.9516,0.937,101590.19,24171.89,0.16,
+     0.008,128775.13,0.15,9,0.2903,0.3676,0.0853,0.001176,0.17,0.0326,3821,862,0.5],
+    ['Skysmart Exams',41348,0.1210,0.7628,0.9463,0.937,91172.79,24851.56,0.08,
+     0.008,98266.09,0.15,9,0.2903,0.3676,0.0853,0.002848,0.11,0.0197,2250,603,0.76],
+    ['Skysmart Exams Large Classes',19021,0.0879,0.8361,0.9391,0.937,50645.17,6888.04,0.08,
+     0.008,31109.05,0.15,9,0.2903,0.3676,0.0853,0.001824,0.14,0.0356,896,179,0.65]
 ]
 
 columns = ['portfolio','orders','conv','approve_rate','take_rate','CR_internal_loan',
@@ -31,7 +31,7 @@ columns = ['portfolio','orders','conv','approve_rate','take_rate','CR_internal_l
            'FTV_internal_loan','annual_inflation_rate','average_loan_period_months',
            'sm','cogs','ops','ya_split_comission',
            'bank_loan_refunds_share','internal_loan_refunds_share',
-           'bank_payments','installment_payments']
+           'bank_payments','installment_payments','internal_loan_repayment_rate']
 
 df = pd.DataFrame(data, columns=columns)
 df['CR_bank_loan'] = df['approve_rate'] * df['take_rate']
@@ -39,26 +39,32 @@ df['months_inflation_rate'] = (1 + df['annual_inflation_rate']) ** (1/12) - 1
 df['inflation_adjustment_factor'] = (1 + df['months_inflation_rate']) ** (-df['average_loan_period_months'])
 
 # Функция расчета финансовых метрик
-def calculate_financial_metrics(df, include_refunds):
+def calculate_financial_metrics(df, gmv=True, include_refunds=True):
     if include_refunds:
-        df['cash_in_bank_loan'] = df['bank_payments'] * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission']) * (1 - df['bank_loan_refunds_share'])
-        df['cash_in_internal'] = df['installment_payments'] * df['FTV_internal_loan'] * 0.7 * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
-        df['GMV'] = df['cash_in_bank_loan'] + df['cash_in_internal']
-        df['CM2'] = df['GMV'] * (1 - df['sm'] - df['cogs'] - df['ops'])
-        df['CM2_bank_loan'] = df['cash_in_bank_loan'] * (1 - df['sm'] - df['cogs'] - df['ops'])
-        df['CM2_internal_loan'] = df['cash_in_internal'] * (1 - df['sm'] - df['cogs'] - df['ops'])
+        df['cash_in_bank_loan'] = df['bank_payments'] * df['ATV_bank_loan'] * (1 + df['bank_loan_commission']) * (1 - df['bank_loan_refunds_share'])
+        df['cash_in_internal'] = df['installment_payments'] * df['FTV_internal_loan'] * df['internal_loan_repayment_rate'] * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
+        df['GMV'] = df['cash_in_bank_loan'] + df['cash_in_internal_loan']
+        df['CM2_bank_loan'] = df['cash_in_bank_loan'] * (1 - df['sm'] - df['cogs'] - df['ops'] - df['bank_loan_commission'] - df['ya_split_comission'])
+        df['CM2_internal_loan'] = df['cash_in_internal'] * (1 - df['sm'] - df['cogs'] - df['ops'] - df['internal_loan_commission'])
+        df['CM2'] = df['CM2_bank_loan'] + df['CM2_internal_loan']
     else:
-        df['cash_in_bank_loan'] = df['bank_payments'] * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission'])
-        df['cash_in_internal'] = df['installment_payments'] * df['FTV_internal_loan'] * 0.7 * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor']
-        df['GMV'] = df['cash_in_bank_loan'] + df['cash_in_internal']
-        df['CM2'] = df['GMV'] * (1 - df['sm'] - df['cogs'] - df['ops'])
-        df['CM2_bank_loan'] = df['cash_in_bank_loan'] * (1 - df['sm'] - df['cogs'] - df['ops'])
-        df['CM2_internal_loan'] = df['cash_in_internal'] * (1 - df['sm'] - df['cogs'] - df['ops'])
-
+        df['cash_in_bank_loan'] = df['bank_payments'] * df['ATV_bank_loan'] * (1 + df['bank_loan_commission'])
+        df['cash_in_internal_loan'] = df['installment_payments'] * df['FTV_internal_loan'] * df['internal_loan_repayment_rate'] * df['inflation_adjustment_factor']
+        df['GMV'] = df['cash_in_bank_loan'] + df['cash_in_internal_loan']
+        df['CM2_bank_loan'] = df['cash_in_bank_loan'] * (1 - df['sm'] - df['cogs'] - df['ops'] - df['bank_loan_commission'] - df['ya_split_comission'])
+        df['CM2_internal_loan'] = df['cash_in_internal_loan'] * (1 - df['sm'] - df['cogs'] - df['ops'] - df['internal_loan_commission'])
+        df['CM2'] = df['CM2_bank_loan'] + df['CM2_internal_loan']
+    
+    df['bank_loan_share_of_gmv'] = round(df['cash_in_bank_loan'] * 100 / df['GMV'], 2)
+    df['internal_loan_share_of_gmv'] = round(df['cash_in_internal_loan'] * 100 / df['GMV'], 2)
     df['bank_loan_share_of_cm2'] = round(df['CM2_bank_loan'] * 100 / df['CM2'], 2)
     df['internal_loan_share_of_cm2'] = round(df['CM2_internal_loan'] * 100 / df['CM2'], 2)
     
-    dff = df[['portfolio','CM2_bank_loan','CM2_internal_loan','CM2','bank_loan_share_of_cm2','internal_loan_share_of_cm2']]
+    if gmv:
+        dff = df[['portfolio','cash_in_bank_loan','cash_in_internal_loan','GMV','bank_loan_share_of_gmv','internal_loan_share_of_gmv']]
+    else:
+        dff = df[['portfolio','CM2_bank_loan','CM2_internal_loan','CM2','bank_loan_share_of_cm2','internal_loan_share_of_cm2']]
+    
     return dff
 
 # Функция расчета GMV без компонента оплаты картой
@@ -70,11 +76,11 @@ def calculate_GMV_no_card(df, weights, internal_loan_repayment_rate, include_ref
     orders_internal_loan = df['orders'] * C1 * weight_internal_loan
     
     if include_refunds:
-        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission']) * (1 - df['bank_loan_refunds_share'])
-        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
+        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 + df['bank_loan_commission']) * (1 - df['bank_loan_refunds_share'])
+        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
     else:
-        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission'])
-        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor']
+        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 + df['bank_loan_commission'])
+        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * df['inflation_adjustment_factor']
     
     GMV = adjusted_cash_in_bank_loan + adjusted_cash_in_internal_loan
     
@@ -89,21 +95,18 @@ def calculate_CM2(df, weights, internal_loan_repayment_rate, include_refunds):
     orders_internal_loan = df['orders'] * C1 * weight_internal_loan
     
     if include_refunds:
-        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission']) * (1 - df['bank_loan_refunds_share'])
-        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
-        adjusted_cm2_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission']) * (1 - df['bank_loan_refunds_share']) * (1 - df['sm'] - df['cogs'] - df['ops'])
-        adjusted_cm2_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share']) * (1 - df['sm'] - df['cogs'] - df['ops'])
+        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 + df['bank_loan_commission']) * (1 - df['bank_loan_refunds_share'])
+        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * df['inflation_adjustment_factor'] * (1 - df['internal_loan_refunds_share'])
+        adjusted_cm2_bank_loan = adjusted_cash_in_bank_loan * (1 - df['sm'] - df['cogs'] - df['ops'] - df['bank_loan_commission'] - df['ya_split_comission'])
+        adjusted_cm2_internal_loan = adjusted_cash_in_internal_loan * (1 - df['sm'] - df['cogs'] - df['ops'] - df['internal_loan_commission'])
     else:
-        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission'])
-        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor']
-        adjusted_cm2_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 - df['bank_loan_commission'] - df['ya_split_comission']) * (1 - df['sm'] - df['cogs'] - df['ops'])
-        adjusted_cm2_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * (1 - df['internal_loan_commission']) * df['inflation_adjustment_factor'] * (1 - df['sm'] - df['cogs'] - df['ops'])    
-    
-    # Расчет GMV
-    GMV = adjusted_cash_in_bank_loan + adjusted_cash_in_internal_loan
+        adjusted_cash_in_bank_loan = orders_bank_loan * df['ATV_bank_loan'] * (1 + df['bank_loan_commission'])
+        adjusted_cash_in_internal_loan = orders_internal_loan * df['FTV_internal_loan'] * internal_loan_repayment_rate * df['inflation_adjustment_factor']
+        adjusted_cm2_bank_loan = adjusted_cash_in_bank_loan * (1 - df['sm'] - df['cogs'] - df['ops'] - df['bank_loan_commission'] - df['ya_split_comission'])
+        adjusted_cm2_internal_loan = adjusted_cash_in_internal_loan * (1 - df['sm'] - df['cogs'] - df['ops'] - df['internal_loan_commission'])   
 
     # Расчет CM2
-    CM2 = GMV * (1 - df['sm'] - df['cogs'] - df['ops'])
+    CM2 = adjusted_cm2_bank_loan + adjusted_cm2_internal_loan
     
     return CM2, adjusted_cm2_bank_loan, adjusted_cm2_internal_loan
 
@@ -119,7 +122,7 @@ def main():
     st.sidebar.write("- [Воронка банковской рассрочки](https://datalens.yandex.cloud/bbs9wgyas0ns0-voronka-bankovskoy-rassrochki)")
     st.sidebar.write("- [Acquisition Funnel B2C](https://datalens.yandex.cloud/8adnr8ow39kux-acquisition-funnel-b2c)")
     st.sidebar.write("- [Consolidated 2024-25](https://docs.google.com/spreadsheets/d/1Q9RP1wJOTvEzOHFshKEWOzWJsJpPTNI2f2uIULBNvtU/edit?gid=1753026548#gid=1753026548)")
-    st.sidebar.write("- [Consolidated 24-25](https://docs.google.com/spreadsheets/d/1g3mAswh1iweDZHsmRMBM5NjkT2va7ITUmFv_wXM8hfY/edit?gid=245601635#gid=245601635)")
+    st.sidebar.write("- [Consolidated 23-24](https://docs.google.com/spreadsheets/d/1g3mAswh1iweDZHsmRMBM5NjkT2va7ITUmFv_wXM8hfY/edit?gid=245601635#gid=245601635)")
 
     st.sidebar.write("### Названия полей")
     st.sidebar.write("'portfolio' - портфель")
@@ -144,6 +147,7 @@ def main():
     st.sidebar.write("'internal_loan_refunds_share' - доля возвратов для внутренней рассрочки")
     st.sidebar.write("'bank_payments' - количество платежей по банковской рассрочке")
     st.sidebar.write("'installment_payments' - количество платежей по внутренней рассрочке")
+    st.sidebar.write("'internal_loan_repayment_rate' - фактическая выплачиваемость внутренней рассрочки")
     st.sidebar.write("'months_inflation_rate' - месячная инфляция")
     st.sidebar.write("'inflation_adjustment_factor' - поправка на инфляцию")
     
